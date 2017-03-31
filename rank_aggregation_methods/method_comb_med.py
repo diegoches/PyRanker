29305c6ab3fe886e59216b095e98108873847b28
Diego Alonso Chavez Escalante <ddce.2005@gmail.com>
2017-03-31 17:56:51 -0500

    Preparing comb med algorithm.

--- rank_aggregation_methods/method_comb_med.py
+++ rank_aggregation_methods/method_comb_med.py
@@ -10,7 +10,7 @@ import operator
 # The CombMED algorithm compute the mean of the similarities of each object
 # in each ranked list, and then it order them in descending order.
 # \cite Fox:1994 .
-class RkCombMED(RankAggregation):
+class RankCombMed(RankAggregation):
     # \fn rank
     # The rank function uses the CombMED algorithm to compute a new ranked
     # list for the query object.
@@ -20,35 +20,53 @@ class RkCombMED(RankAggregation):
     # ('sim','id','rank')).
     def rank(self, *tp_param):
 
-        int_tam = len(self.ls_data[0])
-        int_rnk = len(self.ls_data)
-
-        dc_tmp = {}
-
-        # Sums the similarities of each object in a dictionary where each
-        # key is the id of each object.
-        for i in xrange(0, int_rnk):
-            for j in xrange(0, int_tam):
-                str_name = self.ls_data[i][j].get('id')
-                flo_sim = self.ls_data[i][j].get('sim')
-                if str_name not in dc_tmp:
-                    dc_tmp[str_name] = flo_sim
+        temporal_dictionary = {}
+        for current_rank in self.rank_list.ranks:
+            for element in current_rank.rank:
+                if element.id in temporal_dictionary:
+                    temporal_dictionary[element.id] += element.similitude
                 else:
-                    dc_tmp[str_name] = dc_tmp[str_name] + flo_sim
+                    temporal_dictionary[element.id] = element.similitude
 
-        # Get the mean similarity of each object
-        for v in dc_tmp:
-            dc_tmp[v] /= int_rnk
+        ranks_quantity = self.rank_list.ranks_quantity
+        for k, v in temporal_dictionary.items():
+            temporal_dictionary[k] = v/ranks_quantity
 
-        # Orders a list of the objects by its new similarity.
-        ls_sor = sorted(dc_tmp.iteritems(), key=operator.itemgetter(1),
-                        reverse=True)
+        list_of_aggregated_rank = \
+            sorted(temporal_dictionary.iteritems(),
+                   key=operator.itemgetter(1), reverse=True)
 
-        ls_ra = []
+        # --------------------------------------------------------------------
 
-        # Maps the new rank in the output format.
-        for i in xrange(0, int_tam):
-            dc_tp = {'sim': ls_sor[i][1], 'id': ls_sor[i][0], 'rank': i + 1}
-            ls_ra.append(dc_tp)
+        # int_tam = len(self.ls_data[0])
+        # int_rnk = len(self.ls_data)
+        #
+        # dc_tmp = {}
+        #
+        # # Sums the similarities of each object in a dictionary where each
+        # # key is the id of each object.
+        # for i in xrange(0, int_rnk):
+        #     for j in xrange(0, int_tam):
+        #         str_name = self.ls_data[i][j].get('id')
+        #         flo_sim = self.ls_data[i][j].get('sim')
+        #         if str_name not in dc_tmp:
+        #             dc_tmp[str_name] = flo_sim
+        #         else:
+        #             dc_tmp[str_name] = dc_tmp[str_name] + flo_sim
+        #
+        # # Get the mean similarity of each object
+        # for v in dc_tmp:
+        #     dc_tmp[v] /= int_rnk
+        #
+        # # Orders a list of the objects by its new similarity.
+        # ls_sor = sorted(dc_tmp.iteritems(), key=operator.itemgetter(1),
+        #                 reverse=True)
+        #
+        # ls_ra = []
+        #
+        # # Maps the new rank in the output format.
+        # for i in xrange(0, int_tam):
+        #     dc_tp = {'sim': ls_sor[i][1], 'id': ls_sor[i][0], 'rank': i + 1}
+        #     ls_ra.append(dc_tp)
 
-        return ls_ra
+        # return ls_ra
--- rank_structures/rank_list.py
+++ rank_structures/rank_list.py
@@ -4,22 +4,22 @@ from rank_structures.rank import Rank
 class RankList(object):
 
     def __init__(self):
-        self.rank_list = []
-        self.rank_quantity = 0
+        self.ranks = []
+        self.ranks_quantity = 0
 
     def __str__(self):
         str_result = 'Quantity: {quantity} \n\nRanks:\n\n{ranks}'
         rank_list_as_string = ''
         i = 1
-        for l in self.rank_list:
+        for l in self.ranks:
             rank_list_as_string += '- Rank ' + str(i) + ':\n' + str(l) + '\n'
             i += 1
-        return str_result.format(quantity=self.rank_quantity,
+        return str_result.format(quantity=self.ranks_quantity,
                                  ranks=rank_list_as_string)
 
     def add_rank(self, rank):
         if isinstance(rank, Rank):
-            self.rank_list.append(rank)
-            self.rank_quantity += 1
+            self.ranks.append(rank)
+            self.ranks_quantity += 1
         else:
             raise TypeError('rank is not of Rank class')
