18d11eff54beb911a66c8fe1df92055b8900a3c3
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-04-05 23:06:45 -0500

    Fixing RankCombMAX to use new structures.

--- rank_aggregation_methods/method_comb_max.py
+++ rank_aggregation_methods/method_comb_max.py
@@ -8,42 +8,24 @@ import operator
 # This class inherits from the RankAggregation class, and implements the rank() function. 
 # The CombMAX algorithm get the maximum similarity of each object in the ranked lists, and then it order them in descending order.
 # \cite Fox:1994 .
-class RkCombMAX(RankAggregation):
+class RankCombMAX(RankAggregation):
 
     # \fn rank 
     # The rank function uses the CombMAX algorithm to compute a new ranked 
     # list for the query object.
     # \param tp_param The CombMAX algorithm does not use any hyper-parameter.
-    # \return A ranked list with the same structure as the ls_data attribute 
-    # (a list of dictionaries, each dictionary with three keys 
-    # ('sim','id','rank')).
+    # \return A ranked list with the same structure as the ls_data attribute
     def rank(self, *tp_param):
         
-        int_tam = len(self.ls_data[0])
-        int_rnk = len(self.ls_data)
-        
-        dc_tmp = {}
-        
         # Get the maximum similarity for each object.
-        for i in xrange(0, int_rnk):
-            for j in xrange(0, int_tam):
-                str_name = self.ls_data[i][j].get('id')
-                flo_sim = self.ls_data[i][j].get('sim')
-                if not dc_tmp.has_key(str_name):
-                    dc_tmp[str_name] = flo_sim
+        temporal_dictionary = {}
+        for current_rank in self.rank_list.ranks:
+            for element in current_rank.rank:
+                if element.id in temporal_dictionary:
+                    if element.similitude > temporal_dictionary[element.id]:
+                        temporal_dictionary[element.id] = element.similitude
                 else:
-                    if flo_sim > dc_tmp[str_name]:
-                        dc_tmp[str_name] = flo_sim
-        
-        # Orders a list of the objects by its new similarity.
-        ls_sor = sorted(dc_tmp.iteritems(), key=operator.itemgetter(1),
-                        reverse=True)
-        
-        ls_ra = []
-        
-        # Maps the new rank in the output format.
-        for i in xrange(0,int_tam):
-            dc_tp = {'sim': ls_sor[i][1],'id': ls_sor[i][0],'rank': i+1}
-            ls_ra.append(dc_tp)
-        
-        return ls_ra
+                    temporal_dictionary[element.id] = element.similitude
+
+        self.aggregated_rank.add_dictionary(temporal_dictionary)
+        return self.aggregated_rank
--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 from loaders.load_from_file import LoadFromFile
+from rank_aggregation_methods.method_comb_max import RankCombMAX
 from rank_aggregation_methods.method_comb_med import RankCombMED
 from rank_aggregation_methods.method_comb_min import RankCombMIN
 
@@ -15,6 +16,10 @@ def main():
     print ra.rank()
     print '----'
     rm = RankCombMIN()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '----'
+    rm = RankCombMAX()
     rm.load_rank_list(rl)
     print rm.rank()
     print '****'
