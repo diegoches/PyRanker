b1e753be396292f0fa391f5b8d76c30e399cf7c9
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-04-25 21:39:20 -0500

    Refactoring comments in borda method.

--- rank_aggregation_methods/method_borda.py
+++ rank_aggregation_methods/method_borda.py
@@ -20,47 +20,21 @@ class RankBorda(RankAggregation):
 
         dict_mapper = {}
 
+        # initialize dictionary of ranks from first ranked list.
         for e in self.rank_list.ranks[0].rank:
             dict_mapper[e.id] = e.rank
 
+        # Computes Borda count.
         for i in xrange(1, self.rank_list.ranks_quantity):
             for re in self.rank_list.ranks[i].rank:
                 dict_mapper[re.id] += re.rank
 
         max_count = max(dict_mapper.values())
 
+        # The new similarity is the normalized Borda count.
         for k, v in dict_mapper.items():
             final_count = 1 - (v/float(max_count))
             dict_mapper[k] = final_count
 
         self.aggregated_rank.add_dictionary(dict_mapper)
         return self.aggregated_rank
-
-
-        # -------------
-
-        # dc_map = {}
-        #
-        # # initialize map of ranks from first ranked list.
-        # for dc_i in self.ls_data[0]:
-        #     dc_map.update({dc_i.get('id'): dc_i.get('rank')})
-        #
-        # # Computes Borda count.
-        # for i in xrange(1, self.int_num_ranks):
-        #     for dc_i in self.ls_data[i]:
-        #         dc_map[dc_i.get('id')] += dc_i.get('rank')
-        #
-        # max_count = max(dc_map.values())
-        #
-        # # Sorting objects by using Borda count.
-        # dc_map = sorted(dc_map.items(), key=lambda dc_map: dc_map[1])
-        #
-        # ls_final_rank = []
-        #
-        # # The new similarity is the normalized Borda count.
-        # for i in xrange(len(dc_map)):
-        #     final_count = 1 - dc_map[i][1] / float(max_count)
-        #     ls_final_rank.append({'id': dc_map[i][0],
-        #                          'rank': i + 1, 'sim': final_count})
-        #
-        # return ls_final_rank
