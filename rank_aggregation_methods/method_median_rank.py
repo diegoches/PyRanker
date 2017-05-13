0dfda8717c36382a2890a66c141e8a438ff03841
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-05-13 02:31:36 -0500

    Refactoring Median Rank method.

--- rank_aggregation_methods/method_median_rank.py
+++ rank_aggregation_methods/method_median_rank.py
@@ -27,50 +27,58 @@ class RankMedianRank(RankAggregation):
             for e in self.rank_list.ranks[i].rank:
                 dict_mapper[e.id].append(e.rank)
 
-        # for k,v in dict_mapper.items():
+        for k, v in dict_mapper.items():
+            dict_mapper[k] = self.get_median(v)
 
+        max_median = max(dict_mapper.values())
+
+        for k, v in dict_mapper.items():
+            dict_mapper[k] = 1 - v/float(max_median)
+
+        self.aggregated_rank.add_dictionary(dict_mapper)
+        return self.aggregated_rank
 
 
         # --------------------------------
 
         # define median type function
-        if self.int_num_ranks % 2 == 0:  # even
-            func_median = self.__median_even
-            self.median_index1 = (self.int_num_ranks / 2) - 1
-            self.median_index2 = self.median_index1 + 1
-        else:  # odd
-            func_median = self.__median_odd
-            self.median_index = ((self.int_num_ranks + 1) / 2) - 1
-
-        dc_map = {}
-
-        # initialize map of ranks from first ranked list
-        for dc_i in self.ls_data[0]:
-            dc_map.update({dc_i.get('id'): [dc_i.get('rank')]})
-
-        # Add positons of others ranked lists
-        for i in range(1, self.int_num_ranks):
-            for dc_i in self.ls_data[i]:
-                dc_map[dc_i.get('id')].append(dc_i.get('rank'))
-
-        # Computes median
-        for str_id in dc_map.iterkeys():
-            dc_map[str_id] = func_median(dc_map[str_id])
-
-        max_median = max(dc_map.values())
-
-        # Sorting objects by using the median values
-        dc_map = sorted(dc_map.items(), key=lambda dc_map: dc_map[1])
-
-        ls_final_rank = []
-
-        # The new similarity is the normalized median.
-        for i in range(len(dc_map)):
-            norm_median = 1 - dc_map[i][1] / float(max_median)
-            ls_final_rank.append(
-                {'id': dc_map[i][0], 'rank': i + 1, 'sim': norm_median})
-
-        return ls_final_rank
+        # if self.int_num_ranks % 2 == 0:  # even
+        #     func_median = self.__median_even
+        #     self.median_index1 = (self.int_num_ranks / 2) - 1
+        #     self.median_index2 = self.median_index1 + 1
+        # else:  # odd
+        #     func_median = self.__median_odd
+        #     self.median_index = ((self.int_num_ranks + 1) / 2) - 1
+        #
+        # dc_map = {}
+        #
+        # # initialize map of ranks from first ranked list
+        # for dc_i in self.ls_data[0]:
+        #     dc_map.update({dc_i.get('id'): [dc_i.get('rank')]})
+        #
+        # # Add positons of others ranked lists
+        # for i in range(1, self.int_num_ranks):
+        #     for dc_i in self.ls_data[i]:
+        #         dc_map[dc_i.get('id')].append(dc_i.get('rank'))
+        #
+        # # Computes median
+        # for str_id in dc_map.iterkeys():
+        #     dc_map[str_id] = func_median(dc_map[str_id])
+        #
+        # max_median = max(dc_map.values())
+        #
+        # # Sorting objects by using the median values
+        # dc_map = sorted(dc_map.items(), key=lambda dc_map: dc_map[1])
+        #
+        # ls_final_rank = []
+        #
+        # # The new similarity is the normalized median.
+        # for i in range(len(dc_map)):
+        #     norm_median = 1 - dc_map[i][1] / float(max_median)
+        #     ls_final_rank.append(
+        #         {'id': dc_map[i][0], 'rank': i + 1, 'sim': norm_median})
+        #
+        # return ls_final_rank
 
     # \fn __odd
     # Calculates the median for an odd number of values.
