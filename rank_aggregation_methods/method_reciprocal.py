80d2db92a3220117f8e2e52d02c41134d14e038f
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-05-16 00:28:09 -0500

    Removing unnecessary comments

--- rank_aggregation_methods/method_reciprocal.py
+++ rank_aggregation_methods/method_reciprocal.py
@@ -41,36 +41,3 @@ class RankReciprocal(RankAggregation):
 
         self.aggregated_rank.add_dictionary(dict_mapper)
         return self.aggregated_rank
-
-
-
-        # if tp_param < 0:
-        #     raise ValueError("tp_param must be greater than or equal to zero.")
-        #
-        # dc_map = {}
-        #
-        # # Initialize map of ranks from first ranked list
-        # for dc_i in self.ls_data[0]:
-        #     dc_map.update({dc_i.get('id'):1.0/(tp_param + dc_i.get('rank'))})
-        #
-        # # Computes RRF count
-        # for i in range(1, self.int_num_ranks):
-        #     for dc_i in self.ls_data[i]:
-        #         dc_map[dc_i.get('id')] += 1.0/(tp_param + dc_i.get('rank'))
-        #
-        # max_count = max(dc_map.values())
-        #
-        # # Sorting objects by using RRF count
-        # dc_map = \
-        #     sorted(dc_map.items(), key=lambda dc_map: dc_map[1], reverse=True)
-        #
-        # ls_finalRank = []
-        #
-        # # The new similarity is the normalized RRF count.
-        # for i in range(len(dc_map)):
-        #     final_count = dc_map[i][1]/float(max_count)
-        #     ls_finalRank.append(
-        #         {'id': dc_map[i][0],'rank': i+1, 'sim': final_count})
-        #
-        # return ls_finalRank
-        #
