fa3d2a7b77e82c0942ca994093e865365d2c88b8
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-05-25 21:38:23 -0500

    Commenting old cropland code.

--- rank_aggregation_methods/method_copeland.py
+++ rank_aggregation_methods/method_copeland.py
@@ -60,54 +60,54 @@ class RankCopeland(RankAggregation):
 
         # ----------------------------------------------------------
 
-        dc_data = {}
-        int_size = len(self.ls_data[0])
-        
-        # Create the score matrix
-        matrix = np.zeros((int_size, int_size))
+        # dc_data = {}
+        # int_size = len(self.ls_data[0])
+        #
+        # # Create the score matrix
+        # matrix = np.zeros((int_size, int_size))
+        #
+        # # Initialize the score matrix (comparisons) and compute the scores
+        # # for the first rank aggregation
+        # for dc_i in self.ls_data[0]:
+        #     dc_data.update({dc_i.get("id"): dc_i.get("rank")})
+        #     for int_column in range(dc_i.get("rank"), int_size):
+        #         matrix[dc_i.get("rank")-1][int_column] += 1
+        #
+        # # Compute the comparisons for the others ranked list
+        # for int_i in range(1, self.int_num_ranks):
+        #     ls_winners = []
+        #     for dc_i in self.ls_data[int_i]:
+        #         int_row = dc_data.get(dc_i.get("id")) - 1
+        #         for int_column in range(int_size):
+        #             if int_column not in ls_winners and int_row != int_column:
+        #                 matrix[int_row][int_column] += 1
+        #         ls_winners.append(int_row)
+        #
+        # # Compute the points for each element according to the score matrix
+        # ls_pontos = [0] * int_size
+        # for int_row in range(int_size):
+        #     for int_column in range(int_row+1, int_size):
+        #         if matrix[int_row][int_column] > self.int_num_ranks/2.0:
+        #             ls_pontos[int_row] += 1
+        #         else:
+        #             ls_pontos[int_column] += 1
+        #
+        # # Build rank aggregated list result and compute the similarity value
+        # ls_result = []
+        # int_max = max(ls_pontos)
+        # for int_i in range(int_size):
+        #     ls_result.append(self.ls_data[0][int_i].copy())
+        #     ls_result[int_i]["sim"] = ls_pontos[int_i] / float(int_max)
+        #
+        # # Sort the result list according to similarity
+        # ls_result = sorted(ls_result, key=lambda ls_result: ls_result['sim'],
+        #                    reverse=True)
+        #
+        # # Update the rank values
+        # for int_idx in range(len(ls_result)):
+        #     ls_result[int_idx]["rank"] = int_idx + 1
 
-        # Initialize the score matrix (comparisons) and compute the scores
-        # for the first rank aggregation
-        for dc_i in self.ls_data[0]:
-            dc_data.update({dc_i.get("id"): dc_i.get("rank")})
-            for int_column in range(dc_i.get("rank"), int_size):
-                matrix[dc_i.get("rank")-1][int_column] += 1
-
-        # Compute the comparisons for the others ranked list
-        for int_i in range(1, self.int_num_ranks):
-            ls_winners = []
-            for dc_i in self.ls_data[int_i]:
-                int_row = dc_data.get(dc_i.get("id")) - 1
-                for int_column in range(int_size):
-                    if int_column not in ls_winners and int_row != int_column:
-                        matrix[int_row][int_column] += 1
-                ls_winners.append(int_row)
-        
-        # Compute the points for each element according to the score matrix
-        ls_pontos = [0] * int_size
-        for int_row in range(int_size):
-            for int_column in range(int_row+1, int_size):
-                if matrix[int_row][int_column] > self.int_num_ranks/2.0:
-                    ls_pontos[int_row] += 1
-                else:
-                    ls_pontos[int_column] += 1
-        
-        # Build rank aggregated list result and compute the similarity value
-        ls_result = []
-        int_max = max(ls_pontos)
-        for int_i in range(int_size):
-            ls_result.append(self.ls_data[0][int_i].copy())
-            ls_result[int_i]["sim"] = ls_pontos[int_i] / float(int_max)
-        
-        # Sort the result list according to similarity
-        ls_result = sorted(ls_result, key=lambda ls_result: ls_result['sim'],
-                           reverse=True)
-
-        # Update the rank values
-        for int_idx in range(len(ls_result)):
-            ls_result[int_idx]["rank"] = int_idx + 1
-
-        return ls_result
+        # return ls_result
 
     # \fn __win
     # The win function returns which object is more similar (better position)
