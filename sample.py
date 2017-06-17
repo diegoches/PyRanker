6f65143bac19988d94279225cd654e085c03ce39
Diego Alonso Chávez Escalante <ddce.2005@gmail.com>
2017-06-16 19:12:44 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -23,64 +23,63 @@ def main():
     rl = LoadFromFile.load_ranks(datasets)
     print rl
 
-    # ra = LoadFromFile.load_single_file('datasets/sample_4.txt')
-    # print ra
-    # ra.process_rank()
-    # print ra
+    ra = LoadFromFile.load_single_file('datasets/sample_4.txt')
+    print ra
+    ra.process_rank()
+    print ra
 
-    # print '--RankCombMED--'
-    # ra = RankCombMED()
-    # ra.load_rank_list(rl)
-    # print ra.rank()
-    # print '--RankCombMIN--'
-    # rm = RankCombMIN()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankCombMAX--'
-    # rm = RankCombMAX()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankCombMNZ--'
-    # rm = RankCombMNZ()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankCombSUM--'
-    # rm = RankCombSUM()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankCombANZ--'
-    # rm = RankCombANZ()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankFootrule--'
-    # rm = RankFootrule()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankBorda--'
-    # rm = RankBorda()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankMScores--'
-    # rm = RankMScores()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankMedianRank--'
-    # rm = RankMedianRank()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '--RankReciprocal 0--'
-    # rm = RankReciprocal()
-    # rm.load_rank_list(rl)
-    # print rm.rank(0)
-    # print '--RankReciprocal 1--'
-    # rm = RankReciprocal()
-    # rm.load_rank_list(rl)
-    # print rm.rank(1)
-    # print '--RankCopeland--'
-    # rm = RankCopeland()
-    # rm.load_rank_list(rl)
-    # print rm.rank()
-    # print '****'
-    # print rl
+    print '--RankCombMED--'
+    ra = RankCombMED()
+    ra.load_rank_list(rl)
+    print ra.rank()
+    print '--RankCombMIN--'
+    rm = RankCombMIN()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankCombMAX--'
+    rm = RankCombMAX()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankCombMNZ--'
+    rm = RankCombMNZ()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankCombSUM--'
+    rm = RankCombSUM()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankCombANZ--'
+    rm = RankCombANZ()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankFootrule--'
+    rm = RankFootrule()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankBorda--'
+    rm = RankBorda()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankMScores--'
+    rm = RankMScores()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankMedianRank--'
+    rm = RankMedianRank()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '--RankReciprocal 0--'
+    rm = RankReciprocal()
+    rm.load_rank_list(rl)
+    print rm.rank(0)
+    print '--RankReciprocal 1--'
+    rm = RankReciprocal()
+    rm.load_rank_list(rl)
+    print rm.rank(1)
+    print '--RankCopeland--'
+    rm = RankCopeland()
+    rm.load_rank_list(rl)
+    print rm.rank()
+    print '****************'
 
 main()
