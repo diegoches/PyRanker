a5cbc1439de596be74f987f07d8725ceca4045b2
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-07-13 19:30:33 -0500

    Updating comment in function.

--- rank_aggregation_methods/rank_aggregation.py
+++ rank_aggregation_methods/rank_aggregation.py
@@ -40,11 +40,14 @@ class RankAggregation(object):
         """
         return len(self.rank_list.ranks[0])
 
-    # \fn rank
-    # This function is the one that accomplish the Rank Aggregation algorithm.
-    # It has to be implemented by all the classes that inherit from this class.
-    # \param tp_param is a tuple of hyper-parameters for the
-    # specific algorithm. In the case that the algorithm does not have
-    # hyper-parameters, tp_param would be empty.
     def rank(self, *tp_param):
+        """
+        This function is the one that accomplish the Rank Aggregation
+        algorithm. It has to be implemented by all the classes that inherit
+        from this class.
+
+        :param tp_param: A tuple of hyper-parameters for the specific
+        algorithm. In the case that the algorithm does not have
+        hyper-parameters, tp_param would be empty.
+        """
         raise NotImplementedError("Please Implement this method")
